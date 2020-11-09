c82304ff766408af80f1c6939bfe6ab9de08be5b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-09 09:27:23 +0700

    Add linear time complexity example

---
+++ src/TheBigONotation/Linear.java
@@ -0,0 +1,11 @@
+package TheBigONotation;
+
+/**
+ * Time Complexity 0(n) / Linear
+ */
+public class Linear {
+    public void main(int[] numbers) {
+        for (int number : numbers)
+            System.out.println(number);
+    }
+}
