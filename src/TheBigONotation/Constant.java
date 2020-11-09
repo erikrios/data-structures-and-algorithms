722fc701c45df6c1d54ed1a8811b24769f5e18ee
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-11-09 09:25:06 +0700

    Add constant time complexity example

---
+++ src/TheBigONotation/Constant.java
@@ -0,0 +1,10 @@
+package TheBigONotation;
+
+/**
+ * Time Complexity 0(1) / Constant
+ */
+public class Constant {
+    public void log(int[] numbers) {
+        System.out.println(numbers[0]);
+    }
+}
