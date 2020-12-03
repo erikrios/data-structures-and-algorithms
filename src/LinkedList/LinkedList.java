d70d7d839377e51dd2fd804854f947be780dc136
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-03 08:39:18 +0700

    Add getKthFromEnd method

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -116,6 +116,25 @@ public class LinkedList {
         first = previous;
     }
 
+    public int getKthFromEnd(int k) {
+        if (isEmpty())
+            throw new IllegalArgumentException();
+
+        Node a = first;
+        Node b = first;
+        for (int i = 0; i < k - 1; i++) {
+            b = b.next;
+            if (b == null)
+                throw new IllegalArgumentException();
+        }
+        while (b != last) {
+            a = a.next;
+            b = b.next;
+        }
+
+        return a.value;
+    }
+
     private boolean isEmpty() {
         return first == null;
     }
