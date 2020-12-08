b7b81e47cdf430d330a30b2b10be2d61160c73aa
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-08 20:07:24 +0700

    Create Main.java

---
+++ src/Stack/Main.java
@@ -0,0 +1,9 @@
+package Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Erik Rio Setiawan";
+        StringReverser reverser = new StringReverser();
+        System.out.println(reverser.reverse(name));
+    }
+}
