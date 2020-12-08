d6c9225c7c2166da5fee61050bd080cc63ecf2cc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-08 22:31:51 +0700

    Implements Stack data structure

--- src/Stack/Main.java
+++ src/Stack/Main.java
@@ -9,5 +9,16 @@ public class Main {
         String balancedString = "(([1] + <2>)) [a]";
         Expression expression = new Expression();
         System.out.println(expression.isBalanced(balancedString));
+
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        int popValue = stack.pop();
+        int peekValue = stack.peek();
+        System.out.println(popValue);
+        System.out.println(peekValue);
+        System.out.println(stack.isEmpty());
+        System.out.println(stack);
     }
 }
