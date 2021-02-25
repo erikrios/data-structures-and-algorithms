934bdb49c408b4b0b601c96756428fc1adb86605
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-25 10:24:22 +0700

    Add the implementation of HashSet

--- src/HashTables/Main.java
+++ src/HashTables/Main.java
@@ -1,7 +1,9 @@
 package HashTables;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,5 +21,11 @@ public class Main {
         String name = "Erik Rio Setiawan";
         char firstNonRepeatedNameCharacter = Utils.findTheFirstNonRepeatedCharacter(name);
         System.out.println(firstNonRepeatedNameCharacter);
+
+        Set<Integer> set = new HashSet<>();
+        int[] numbers = {1, 2, 3, 3, 2, 1, 4};
+        for (int number : numbers)
+            set.add(number);
+        System.out.println(set);
     }
 }
