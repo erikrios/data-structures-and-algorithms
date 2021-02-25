0c1f29a1be2b9fcadd73854f7017c0e81658cbdc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-25 10:35:14 +0700

    Add a method to find the first repeated character

--- src/HashTables/Utils.java
+++ src/HashTables/Utils.java
@@ -1,7 +1,9 @@
 package HashTables;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Utils {
     public static char findTheFirstNonRepeatedCharacter(String sentences) {
@@ -17,6 +19,20 @@ public class Utils {
         for (char character : chars) {
             if (map.get(character) == 1)
                 return character;
+        }
+
+        return Character.MIN_VALUE;
+    }
+
+    public static char findTheFirstRepeatedCharacter(String sentences) {
+        Set<Character> set = new HashSet<>();
+        String lowerCaseSentences = sentences.toLowerCase();
+        char[] chars = lowerCaseSentences.toCharArray();
+
+        for (char character : chars) {
+            if (set.contains(character))
+                return character;
+            set.add(character);
         }
 
         return Character.MIN_VALUE;
