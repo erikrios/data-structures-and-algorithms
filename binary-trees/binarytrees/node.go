f47d2809a498f57ceccc365ac861af178c88aa44
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-07-10 19:39:02 +0700

    Create the node.go file

---
+++ binary-trees/binarytrees/node.go
@@ -0,0 +1,22 @@
+package binarytrees
+
+type Number interface {
+	uint8 |
+	uint16 |
+	uint32 |
+	uint64 |
+	uint |
+	int8 |
+	int16 |
+	int32 |
+	int64 |
+	int |
+	float32 |
+	float64
+}
+
+type node[T Number] struct {
+	leftChild *node[T]
+	rightChild *node[T]
+	val T
+}
